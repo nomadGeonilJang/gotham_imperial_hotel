c932bfe59a4fc8dd6c8ac0882801abca98c48a25
JangGeonil <wkdrjsdlf2@gmail.com>
2020-10-17 16:52:16 +0900

    fetch error catch logic

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,11 +1,6 @@
 console.log("Hello world");
 self.addEventListener("fetch", (event) => {
-  if (event.request.url.includes("bootstrap.min.css")) {
-    event.respondWith(
-      new Response(
-        ".hotel-slogan {background:green !important} nav{display:none}",
-        { headers: { "Content-Type": "text/css" } }
-      )
-    );
-  }
+  event.respondWith(
+    fetch(event.request).catch(() => fetch("/index-offline.html"))
+  );
 });
